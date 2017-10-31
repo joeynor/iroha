3950110194229da1c4377de87c9ca60b4529e2ae
motxx <motohiko.ave@gmail.com>
2017-10-31 14:38:11 +0900

    Add shared_model interface AssetResponse
    Signed-off-by: motxx <motohiko.ave@gmail.com>

---
+++ shared_model/interfaces/queries/responses/asset_response.hpp
@@ -0,0 +1,77 @@
+/**
+ * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IROHA_SHARED_MODEL_ASSET_RESPONSE_HPP
+#define IROHA_SHARED_MODEL_ASSET_RESPONSE_HPP
+
+#include "interfaces/asset.hpp"
+#include "interfaces/common_objects/types.hpp"
+#include "interfaces/hashable.hpp"
+#include "interfaces/queries/responses/asset_response.hpp"
+#include "model/queries/responses/asset_response.hpp"
+
+namespace shared_model {
+  namespace interface {
+    /**
+     * Provide response with asset
+     */
+    class AssetResponse
+        : public Hashable<AssetResponse, iroha::model::AssetResponse> {
+     public:
+      /**
+       * @return Attached asset
+       */
+      virtual const Asset &asset() const = 0;
+
+      /**
+       * Stringify the data.
+       * @return string representation of data.
+       */
+      virtual std::string toString() const override {
+        std::string s = "AssetResponse:{";
+        s += "Asset:" + asset().toString();
+        s += "}";
+        return s;
+      }
+
+      /**
+       * @return hash of object.
+       * Equality of hashes means equality of objects.
+       */
+      virtual const HashType &hash() const override;
+
+      /**
+       * operator ==
+       * @return true if the data are same.
+       */
+      virtual bool operator==(const ModelType &rhs) const override {
+        return asset() == rhs.asset();
+      }
+
+      /**
+       * Makes old model.
+       * @return An allocated old model of asset response.
+       */
+      [[deprecated]] virtual OldModelType *makeOldModel() const override {
+        OldModelType *oldModel = new OldModelType();
+        oldModel->asset = *asset().makeOldModel();
+        return oldModel;
+      }
+    };
+  }  // namespace interface
+}  // namespace shared_model
+#endif  // IROHA_SHARED_MODEL_ASSET_RESPONSE_HPP
